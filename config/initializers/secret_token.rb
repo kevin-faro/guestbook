de32a7e4f93bb1ad2328ecdb707483ed5feabb5c
Kevin Faro <kevin.faro@gmail.com>
2012-10-18 18:36:02 -0400

    result of rails new guestbook --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = 'cfbe7851d0cb520f2bdb3de13d42852278b5e0f3b7e5a46f049386c4436119ff30f9a4d09472cd40314a1b0aac055ed3e24394115e61a31657a714a8af809c35'
